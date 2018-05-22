c3ca8550d6d39ea389c6716d7e8ac3b8daed917e
Daning <simonszydlik@gmail.com>
2018-05-22 13:38:27 +0200

    [Added] TestServer, TestClient
    leuk dit

--- app/src/sockets/Client.java
+++ app/src/sockets/Client.java
@@ -9,13 +9,13 @@ import java.net.UnknownHostException;
 
 public class Client {
     public static void main(String[] args) throws UnknownHostException, IOException {
-        String name = args[0];
+        //String name = args[0];
         Socket socket = new  Socket("localhost", 4444);
         PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
         BufferedReader bufferedReader = new java.io.BufferedReader(new InputStreamReader(System.in));
         while(true){
             String readerInput = bufferedReader.readLine();
-            printWriter.println(name +": "+readerInput);
+            printWriter.println(": "+readerInput);
         }
     }
 }
