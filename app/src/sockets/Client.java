fa9498b445620d1ec6cb76f6d175567531732b24
Daning <simonszydlik@gmail.com>
2018-05-22 15:33:19 +0200

    [Changed] Server, Client
    Beetje rondkutten

--- app/src/sockets/Client.java
+++ app/src/sockets/Client.java
@@ -1,21 +1,113 @@
 package sockets;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
+/**
+ * A simple Swing-based client for the capitalization server.
+ * It has a main frame window with a text field for entering
+ * strings and a textarea to see the results of capitalizing
+ * them.
+ */
 public class Client {
-    public static void main(String[] args) throws UnknownHostException, IOException {
-        //String name = args[0];
-        Socket socket = new  Socket("localhost", 4444);
-        PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader bufferedReader = new java.io.BufferedReader(new InputStreamReader(System.in));
-        while(true){
-            String readerInput = bufferedReader.readLine();
-            printWriter.println(": "+readerInput);
+
+
+    private static BufferedReader in;
+    private PrintWriter out;
+    private Timer timer;
+    private JFrame frame = new JFrame("Capitalize Client");
+    private JTextField dataField = new JTextField(40);
+    private static JTextArea messageArea = new JTextArea(8, 60);
+
+    /**
+     * Constructs the client by laying out the GUI and registering a
+     * listener with the textfield so that pressing Enter in the
+     * listener sends the textfield contents to the server.
+     */
+    public Client(){
+
+
+
+        // Layout GUI
+        messageArea.setEditable(false);
+        frame.getContentPane().add(dataField, "North");
+        frame.getContentPane().add(new JScrollPane(messageArea), "Center");
+
+        // Add Listeners
+        dataField.addActionListener(new ActionListener() {
+            /**
+             * Responds to pressing the enter key in the textfield
+             * by sending the contents of the text field to the
+             * server and displaying the response from the server
+             * in the text area.  If the response is "." we exit
+             * the whole application, which closes all sockets,
+             * streams and windows.
+             */
+            public void actionPerformed(ActionEvent e) {
+                out.println(dataField.getText());
+                String response;
+                try {
+                    response = in.readLine();
+                    if (response == null || response.equals("")) {
+                        System.exit(0);
+                    }
+                } catch (IOException ex) {
+                    response = "Error: " + ex;
+                }
+                messageArea.append(response + "\n");
+                dataField.selectAll();
+            }
+        });
+    }
+
+    /**
+     * Implements the connection logic by prompting the end user for
+     * the server's IP address, connecting, setting up streams, and
+     * consuming the welcome messages from the server.  The Capitalizer
+     * protocol says that the server sends three lines of text to the
+     * client immediately after establishing a connection.
+     */
+    public void connectToServer() throws IOException {
+
+        // Get the server address from a dialog box.
+        String serverAddress = JOptionPane.showInputDialog(
+                frame,
+                "Enter IP Address of the Server:",
+                "Welcome to the Capitalization Program",
+                JOptionPane.QUESTION_MESSAGE);
+
+        // Make connection and initialize streams
+        Socket socket = new Socket(serverAddress, 9898);
+        in = new BufferedReader(
+                new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+
+
+        // Consume the initial welcoming messages from the server
+        for (int i = 0; i < 3; i++) {
+            messageArea.append(in.readLine() + "\n");
         }
+    }
+
+    public static void update() throws IOException {
+        String message = in.readLine();
+        messageArea.append(message + "\n");
+    }
+
+    /**
+     * Runs the client application.
+     */
+    public static void main(String[] args) throws Exception {
+        Client client = new Client();
+        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        client.frame.pack();
+        client.frame.setVisible(true);
+        client.connectToServer();
     }
 }
--- app/src/sockets/Server.java
+++ app/src/sockets/Server.java
@@ -1,22 +1,152 @@
 package sockets;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Date;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * A server program which accepts requests from clients to
+ * capitalize strings.  When clients connect, a new thread is
+ * started to handle an interactive dialog in which the client
+ * sends in a string and the server thread sends back the
+ * capitalized version of the string.
+ *
+ * The program is runs in an infinite loop, so shutdown in platform
+ * dependent.  If you ran it from a console window with the "java"
+ * interpreter, Ctrl+C generally will shut it down.
+ */
 public class Server {
-    public static final int PORT = 4444;
-    public static void main(String[] args) throws IOException {
-        new Server().runServer();
+
+    private static ArrayList<PrintWriter> writers = new ArrayList<>();
+    private static ArrayList<Capitalizer> caps = new ArrayList<>();
+    /**
+     * Application method to run the server runs in an infinite loop
+     * listening on port 9898.  When a connection is requested, it
+     * spawns a new thread to do the servicing and immediately returns
+     * to listening.  The server keeps a unique client number for each
+     * client that connects just to show interesting logging
+     * messages.  It is certainly not necessary to do this.
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("The capitalization server is running.");
+        int clientNumber = 0;
+        ServerSocket listener = new ServerSocket(9898);
+        try {
+            while (true) {
+                Capitalizer cap = new Capitalizer(listener.accept(), clientNumber++);
+                cap.start();
+                caps.add(cap);
+                System.out.println(caps.size());
+            }
+        } finally {
+            listener.close();
+        }
     }
-    public void runServer() throws IOException{
-        //Maak server socket, en luister voor connections. Meteen accepten.
-        ServerSocket serverSocket = new ServerSocket(PORT);
-        System.out.println("Ready for connection...");
-        //Zorgt ervoor dat de server meerdere en verschillende clients kan accepten.
-        while (true){
-            Socket socket = serverSocket.accept();
-            new ServerThread(socket).start();
+
+    public static void sendMessages(String message){
+        for(Capitalizer cap : caps){
+            cap.print(message);
+
+        }
+    }
+
+    public static void addWriter(PrintWriter writer){
+        writers.add(writer);
+    }
+
+    public ArrayList<PrintWriter> getWriters() {
+        return writers;
+    }
+
+    public void setWriters(ArrayList<PrintWriter> writers) {
+        this.writers = writers;
+    }
+
+    /**
+     * A private thread to handle capitalization requests on a particular
+     * socket.  The client terminates the dialogue by sending a single line
+     * containing only a period.
+     */
+    private static class Capitalizer extends Thread {
+        private Socket socket;
+        private int clientNumber;
+        private PrintWriter out;
+
+        public Capitalizer(Socket socket, int clientNumber) {
+            this.socket = socket;
+            this.clientNumber = clientNumber;
+            log("New connection with client# " + clientNumber + " at " + socket);
+        }
+
+        /**
+         * Services this thread's client by first sending the
+         * client a welcome message then repeatedly reading strings
+         * and sending back the capitalized version of the string.
+         */
+        public void run() {
+            try {
+
+                // Decorate the streams so we can send characters
+                // and not just bytes.  Ensure output is flushed
+                // after every newline.
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream()));
+
+                out = new PrintWriter(socket.getOutputStream(), true);
+                addWriter(out);
+
+                // Send a welcome message to the client.
+                out.println("Hello, you are client #" + clientNumber + ".");
+                out.println("Enter a line with only a period to quit\n");
+
+                // Get messages from the client, line by line; return them
+                // capitalized
+                while (true) {
+                    String input = in.readLine();
+                    if (input == null || input.equals(".")) {
+                        break;
+                    }
+                    String line = input.toUpperCase();
+
+                    sendMessages(line);
+
+                    //out.println(line);
+                    System.out.println(line);
+                }
+            } catch (IOException e) {
+                log("Error handling client# " + clientNumber + ": " + e);
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    log("Couldn't close a socket, what's going on?");
+                }
+                log("Connection with client# " + clientNumber + " closed");
+            }
+        }
+
+        /**
+         * Logs a simple message.  In this case we just write the
+         * message to the server applications standard output.
+         */
+        private void log(String message) {
+            System.out.println(message);
+        }
+
+        private void print(String message){
+            out.println(message);
         }
     }
 }
